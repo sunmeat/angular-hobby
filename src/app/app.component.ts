6152b7633938d207d226b888a61e2402a81f91a8
Oleksandr Zahoruiko <sunmeatrich@gmail.com>
2024-06-26 16:27:48 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HobbyComponent } from './hobby/hobby.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HobbyComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'hobby';
+  title = 'Моё странное хобби';
 }
---
+++ src/app/hobby/hobby.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hobby',
+  standalone: true,
+  imports: [],
+  templateUrl: './hobby.component.html',
+  styleUrl: './hobby.component.css'
+})
+export class HobbyComponent {
+
+}
